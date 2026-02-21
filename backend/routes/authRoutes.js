ac84a31ab2d58033398943f4f5d363e24b6e03b3
Rushikesh Sutar <navopsacademy@gmail.com>
2026-02-21 16:17:15 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,1 +1,9 @@
-// Auth Routes Placeholder
+import express from "express";
+import { register, login } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+
+export default router;// Auth Routes Placeholder

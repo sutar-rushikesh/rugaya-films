8f29119850613bebdf349ec7ebe6d38242ad7f02
Rushikesh Sutar <navopsacademy@gmail.com>
2026-02-21 16:14:48 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,11 @@
-export default {};
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default pool;

b57e736f75bf1441dd299ea40c62a6ceac5de822
Rushikesh Sutar <navopsacademy@gmail.com>
2026-02-21 16:17:46 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,20 @@
-CREATE TABLE users(id SERIAL PRIMARY KEY);
+CREATE DATABASE rugaya;
+
+\c rugaya
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password TEXT,
+    role VARCHAR(20) DEFAULT 'user'
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150),
+    description TEXT,
+    price NUMERIC,
+    image_url TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);

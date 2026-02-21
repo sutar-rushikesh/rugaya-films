2a5c36edf37a9d79c6b42a8091a4a49763be0f18
Rushikesh Sutar <navopsacademy@gmail.com>
2026-02-21 16:14:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('RUGAYA FILMS Backend Ready');
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("RUGAYA FILMS API Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
